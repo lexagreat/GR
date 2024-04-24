1cae31f376d0bcf9e2651a7aca927f852ecf6ae8
zorion <zorion1234@mail.ru>
2024-04-25 00:40:18 +0300

    make portfolio page

--- #src/js/app.js
+++ #src/js/app.js
@@ -53,6 +53,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
    // about
    controlsExampleVideo();
+
+   // portfolio
+   accordion(".faq-item__header", ".faq-item__spoiler");
 });
 
 // Popup
@@ -158,6 +161,21 @@ document.addEventListener("keydown", function (e) {
 
 //swipers
 document.addEventListener("DOMContentLoaded", () => {
+   const portfolioSwiper = new Swiper(".portfolio-hero__swiper", {
+      slidesPerView: 2,
+      spaceBetween: 6,
+
+      navigation: {
+         nextEl: ".portfolio-hero__nav_next",
+         prevEl: ".portfolio-hero__nav_prev",
+      },
+      breakpoints: {
+         993: {
+            spaceBetween: 36,
+            slidesPerView: 3,
+         },
+      },
+   });
    const mainHeroSwiper = new Swiper(".hero__swiper", {
       spaceBetween: 20,
       // grabCursor: true,
@@ -356,4 +374,101 @@ function controlsExampleVideo() {
          video.play();
       }
    });
+}
+function accordion(linkSelector, contentSelector) {
+   // получаем линки
+   const openLinks = document.querySelectorAll(`${linkSelector}`);
+   // контенты
+   const contents = document.querySelectorAll(`${contentSelector}`);
+
+   if (!openLinks.length) {
+      return;
+   }
+   openLinks.forEach((link, i) => {
+      link.addEventListener("click", () => {
+         if (contents[i].classList.contains("collapsing")) {
+            return;
+         }
+         slideToggle(contents[i]);
+         link.classList.toggle("active");
+      });
+   });
+}
+
+function slideShow(el, duration = 300) {
+   // завершаем работу метода, если элемент содержит класс collapsing или collapse_show
+   if (
+      el.classList.contains("collapsing") ||
+      el.classList.contains("collapse_show")
+   ) {
+      return;
+   }
+   // удаляем класс collapse
+   el.classList.remove("collapse");
+   // сохраняем текущую высоту элемента в константу height (это значение понадобится ниже)
+   const height = el.offsetHeight;
+   // устанавливаем высоте значение 0
+   el.style["height"] = 0;
+   // не отображаем содержимое элемента, выходящее за его пределы
+   el.style["overflow"] = "hidden";
+   // создание анимации скольжения с помощью CSS свойства transition
+   el.style["transition"] = `height ${duration}ms ease`;
+   // добавляем класс collapsing
+   el.classList.add("collapsing");
+   // получим значение высоты (нам этого необходимо для того, чтобы просто заставить браузер выполнить перерасчет макета, т.к. он не сможет нам вернуть правильное значение высоты, если не сделает это)
+   el.offsetHeight;
+   // установим в качестве значения высоты значение, которое мы сохранили в константу height
+   el.style["height"] = `${height}px`;
+   // по истечении времени анимации this._duration
+   window.setTimeout(() => {
+      // удалим класс collapsing
+      el.classList.remove("collapsing");
+      // добавим классы collapse и collapse_show
+      el.classList.add("collapse");
+      el.classList.add("collapse_show");
+      // удалим свойства height, transition и overflow
+      el.style["height"] = "";
+      el.style["transition"] = "";
+      el.style["overflow"] = "";
+   }, duration);
+}
+function slideHide(el, duration = 300) {
+   // завершаем работу метода, если элемент содержит класс collapsing или collapse_show
+   if (
+      el.classList.contains("collapsing") ||
+      !el.classList.contains("collapse_show")
+   ) {
+      return;
+   }
+   // установим свойству height текущее значение высоты элемента
+   el.style["height"] = `${el.offsetHeight}px`;
+   // получим значение высоты
+   el.offsetHeight;
+   // установим CSS свойству height значение 0
+   el.style["height"] = 0;
+   // обрежем содержимое, выходящее за границы элемента
+   el.style["overflow"] = "hidden";
+   // добавим CSS свойство transition для осуществления перехода длительностью this._duration
+   el.style["transition"] = `height ${duration}ms ease`;
+   // удалим классы collapse и collapse_show
+   el.classList.remove("collapse");
+   el.classList.remove("collapse_show");
+   // добавим класс collapsing
+   el.classList.add("collapsing");
+   // после завершения времени анимации
+   window.setTimeout(() => {
+      // удалим класс collapsing
+      el.classList.remove("collapsing");
+      // добавим класс collapsing
+      el.classList.add("collapse");
+      // удалим свойства height, transition и overflow
+      el.style["height"] = "";
+      el.style["transition"] = "";
+      el.style["overflow"] = "";
+   }, duration);
+}
+function slideToggle(el, duration = 300) {
+   el.classList.contains("collapse_show")
+      ? slideHide(el, duration)
+      : slideShow(el, duration);
 }
